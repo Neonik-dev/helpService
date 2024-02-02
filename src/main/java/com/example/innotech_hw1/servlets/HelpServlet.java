22d90da1c01ea4296d558ec263dff04737cbeb2c
neonik <lis.gleb2012@gmail.com>
2024-02-02 09:07:25 +0300

    add servlets

---
+++ src/main/java/com/example/innotech_hw1/servlets/HelpServlet.java
@@ -0,0 +1,36 @@
+package com.example.innotech_hw1.servlets;
+
+import com.example.innotech_hw1.services.HelpService;
+import com.example.innotech_hw1.services.IHelpService;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+
+@WebServlet(value = "/help-service/v1/support")
+public class HelpServlet extends HttpServlet {
+    private IHelpService helpService;
+
+    @Override
+    public void init() {
+        helpService = new HelpService();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String phrase = req.getReader().lines().collect(Collectors.joining("\n"));
+        helpService.addPhrase(phrase);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain");
+        try (PrintWriter printWriter = resp.getWriter()) {
+            printWriter.println(helpService.getPhrase());
+            printWriter.flush();
+        }
+    }
+}
---
+++ src/main/java/com/example/innotech_hw1/utils/RandomizedSet.java
@@ -0,0 +1,51 @@
+package com.example.innotech_hw1.utils;
+
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+@NoArgsConstructor
+public class RandomizedSet<E> {
+    private final Random random = new Random();
+    private final Map<E, Integer> dict = new HashMap<>();
+    private final List<E> arr = new ArrayList<>();
+    private int lastIndex = -1;
+
+    @SafeVarargs
+    public RandomizedSet(E... values) {
+        Arrays.stream(values).forEach(this::insert);
+    }
+
+    public boolean insert(E val) {
+        if (dict.containsKey(val)) {
+            return false;
+        }
+
+        arr.add(val);
+        dict.put(val, ++lastIndex);
+        return true;
+    }
+
+    public boolean remove(E val) {
+        if (!dict.containsKey(val)) {
+            return false;
+        }
+        int curIndex = dict.get(val);
+        if (curIndex != lastIndex) {
+            E lastVal = arr.get(lastIndex);
+            arr.set(curIndex, lastVal);
+            dict.put(lastVal, curIndex);
+        }
+        arr.remove(lastIndex);
+        dict.remove(val);
+        lastIndex--;
+        return true;
+    }
+
+    public E getRandom() {
+        if (lastIndex == -1) {
+            throw new NullPointerException("Set is empty!");
+        }
+        return arr.get(random.nextInt(arr.size()));
+    }
+}
